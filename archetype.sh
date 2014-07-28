2f5ce72ef6c52a02953eef5cbf9791d27d555f4f
mgijsberti <mgijsberti@gmail.com>
2014-07-28 14:36:47 +0200

    Arechetype 7.9.1

--- archetype.sh
+++ archetype.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-VERSION='1.09.00'
-#VERSION='1.07.07'
-#VERSION='1.05.09'
+VERSION='1.09.01'
 mvn archetype:generate -DarchetypeRepository=http://maven.onehippo.com/maven2 -DarchetypeGroupId=org.onehippo.cms7 -DarchetypeArtifactId=hippo-archetype-website -DarchetypeVersion=${VERSION}
 
